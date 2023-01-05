b5361f1e9a9f99f8b5a9c141d076e269df00d00c
newton mathias <newtymathias@gmail.com>
2023-01-05 16:49:46 +0300

    main page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
+import CartMenu from "./scenes/global/CartMenu";
+import Footer from "./scenes/global/Footer";
 import Navbar from "./scenes/global/Navbar";
 import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
@@ -28,6 +30,8 @@ function App() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/checkout/success" element={<Confirmation />} />
         </Routes>
+        <CartMenu />
+        <Footer />
       </BrowserRouter>
     </div>
   );
